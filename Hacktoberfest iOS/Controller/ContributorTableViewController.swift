0cf0bc4ae31815562554a4fc92cdb3f71edfbd2c
Swamita <swamita2001@gmail.com>
2020-10-05 21:02:03 +0530

    add tab bar vc

--- Hacktoberfest iOS/Controller/ContributorTableViewController.swift
+++ Hacktoberfest iOS/Controller/ContributorTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SafariServices
 
-class TableViewController: UITableViewController {
+class ContributorTableViewController: UITableViewController {
     
     var cellIdentifier = "cell"
     
---
+++ Hacktoberfest iOS/Controller/FrameworkTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContributorTableViewController.swift
+//  Hacktoberfest iOS
+//
+//  Created by Swamita on 05/10/20.
+//
+
+import UIKit
+
+class FrameworkTableViewController: UITableViewController {
+    
+    var cellIdentifier = "frameworkCell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //TODO initial setup
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
