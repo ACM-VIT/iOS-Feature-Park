d4a9b6575b3e35b0ffffbfdb7786cd2bf7594bb2
Swamita <swamita2001@gmail.com>
2020-10-05 22:01:26 +0530

    Add table view methods

--- Hacktoberfest iOS/Controller/FrameworkTableViewController.swift
+++ Hacktoberfest iOS/Controller/FrameworkTableViewController.swift
@@ -10,21 +10,40 @@ import UIKit
 class FrameworkTableViewController: UITableViewController {
     
     var cellIdentifier = "frameworkCell"
+    var segueIdentifier = "frameworkToDetail"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         //TODO initial setup
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Datasource.frameworks.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        let framework = Datasource.frameworks[indexPath.row]
+        cell.textLabel?.text = framework.name
+        return cell
+    }
+    
+    //MARK: - Table view Delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: segueIdentifier, sender: self)
+    }
+    
+    
+
 }
--- Hacktoberfest iOS/Model/Datasource.swift
+++ Hacktoberfest iOS/Model/Datasource.swift
@@ -12,4 +12,9 @@ struct Datasource {
         .init(name: "Aaryan Kothari", social: [.github(url: "https://github.com/aaryankotharii"),.LinkedIn(url: ""),.mail(url: "www.linkedin.com/in/aaryankotharii")]),
         .init(name: "Aaryan Kothari", social: [.github(url: "https://github.com/aaryankotharii"),.LinkedIn(url: ""),.mail(url: "www.linkedin.com/in/aaryankotharii")])
     ]
+    
+    static let frameworks: [FrameworkModel] = [
+        .init(name: "UIKit", description: "The UIKit framework provides the window and view architecture for implementing the interface of your app."),
+        .init(name: "SafariServices", description: "Safari Services integrates Safari behaviors into your iOS or macOS app.")
+    ]
 }
